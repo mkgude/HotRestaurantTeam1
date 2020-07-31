27136b9f1b02c6ace0abb05a44aa0d0465b3b2c2
Marian Gude <mkgude@gmail.com>
2020-07-31 13:02:22 -0500

    created json files

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-// dependencies
-// ==========================
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes
 // ==========================
 
-// listener
-// =========================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservation", function (req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/waitlist", function (req, res) {
+  res.sendFile(path.join(__dirname, "waitlist.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
